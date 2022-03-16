acc5169b6c06f14ce83232185700ffde83cbdcdf
Ryan Volz <ryan.volz@gmail.com>
2022-03-16 12:17:29 -0400

    Enable linux-aarch64 build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,6 +21,6 @@ else
     )
 fi
 
-cmake .. "${cmake_config_args[@]}"
+cmake ${CMAKE_ARGS} -G "Ninja" .. "${cmake_config_args[@]}"
 cmake --build . --config Release -- -j${CPU_COUNT}
 cmake --build . --config Release --target install
